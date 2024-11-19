e67f3bff6bfd72f4477f56abaf2ca9e3932d2e75
Gustavo Medeiros <136112308+Gustavomedeirost@users.noreply.github.com>
2024-11-19 14:33:35 -0300

    Add files via upload

---
+++ Site Acesa/script.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector('.menu-button');
+const sidebar = document.querySelector('.sidebar');
+
+menuButton.addEventListener('click', () => {
+  console.log('toggle')
+  const expanded = menuButton.getAttribute('aria-expanded') === 'true' || false;
+  menuButton.setAttribute('aria-expanded', !expanded);
+  sidebar.classList.toggle('open');
+})
